a470f421ae73a8f280581fc82284c2643f996ea8
SebastianGarroDelRio <sebastiangarrodelrio@gmail.com>
2023-05-03 11:30:36 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace proyecto_sistema.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
