7b3f2c81ac2fc0c95e544e3e0dac1dcc62d7de12
DexterPai <rrchk.22@gmail.com>
2023-05-06 17:55:25 -0500

    Prueba

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -26,6 +26,7 @@ namespace proyecto_sistema.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            //Hola 
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
